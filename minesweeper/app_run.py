9a455b418d57bab3db0da6b43761061d959d7c48
Prashant Tiwari <Prashant1080p@Gmail.com>
2018-03-13 20:51:33 +0530

    minesweeper_mini

---
+++ minesweeper/app_run.py
@@ -0,0 +1,13 @@
+'''
+Python Version = Python 3.6.2
+GUI = Tkinter
+Programmer = TheZero6
+'''
+
+import tkinter as tk
+from models.MinesWeeper import main
+
+
+
+if __name__ == "__main__":
+    main()
